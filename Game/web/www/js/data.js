f6290b1ce59615fec03963d24e14ce6787b8f621
Boti <botondbudahazi2007@gmail.com>
2026-09-21 19:01:21 +0200

    fix: fetch not working fixed

--- Game/web/www/js/data.js
+++ Game/web/www/js/data.js
@@ -1,7 +1,11 @@
 let BASE_URL = "http://localhost:3000";
 
 export async function fetchDb(dbName) {
-    const fetchDb = await fetch(`${BASE_URL}/${dbName}`);
-    const data = await fetchDb.json();
-    return data;
+    const fetchDb = await fetch(`${BASE_URL}/${dbName}`,{
+        method: "GET",
+        headers: {
+            "Accept": "application/json"
+        }
+    });
+    return await fetchDb.json();
 }
